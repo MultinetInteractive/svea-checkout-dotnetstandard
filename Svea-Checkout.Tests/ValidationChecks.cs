0e50e91122df69c32ec6e05014200e9355f3f084
Chris Gårdenberg <chga@multinet.se>
2021-04-20 17:00:03 +0200

    wip: Added testproject, to make sure that everything works

---
+++ Svea-Checkout.Tests/ValidationChecks.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace Svea_Checkout.Tests
+{
+    public class ValidationChecks
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
